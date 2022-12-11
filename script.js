0aa91f447b96a546079b543808fd6811d7becb8c
Sardor-Abduhamidov007 <sardorabduhamidov007@gmail.com>
2022-12-11 22:37:55 +0500

    initial commit

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-let nameEl = document.getElementById('name');
-let weightEl = document.getElementById('weight');
-let heightEl = document.getElementById('height');
-let msgEl = document.getElementById('title');
-let formEl = document.getElementById('form-body');
+let nameEl = document.getElementById("name");
+let weightEl = document.getElementById("weight");
+let heightEl = document.getElementById("height");
+let msgEl = document.getElementById("title");
+let formEl = document.getElementById("form-body");
 
-form-body.addEventListener('submit', (e)=> {
-    e.preventDefault()
-    let nameBro = nameEl.value;
-    let name = nameBro.charAt(0).toUpperCase() + nameBro.slice(1).toLowerCase();
-    let heightBro = heightEl.value;
-    let height = heightBro / 100;
-    let weight = weightEl.value;
-    let calc = (weight / height ** 2).toFixed(1);
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let nameBro = nameEl.value;
+  let name = nameBro.charAt(0).toUpperCase() + nameBro.slice(1).toLowerCase();
+  let heightBro = heightEl.value;
+  let height = heightBro / 100;
+  let weight = weightEl.value * 1;
+  let calc = (weight / height ** 2).toFixed(1) * 1;
 
-    if (calc <= 18.5) {
-        msgEl.texContent = `${name} you are thin person !`
-    } else if (18.5 < calc && calc < 24.9) {
-        msgEl.texContent = `${name} you are normal person !`
-    } else if (25 < calc && calc < 29.9) {
-        msgEl.texContent = `${name} you are fat person  !`
-    } else {
-        msgEl.texContent = `${name} you are very fat person !`
-    }
-    nameEl.value = ''
-    heightEl.value = ''
-    weightEl.value = ''
-})
+  if (calc <= 18.5) {
+    msgEl.textContent = `${name} you are thin person !`;
+    let utter = new SpeechSynthesisUtterance();
+    utter.lang = "en-US";
+    utter.text = `${name} you are thin person !`;
+    utter.volume = 0.7;
+    window.speechSynthesis.speak(utter);
+  } else if (18.5 < calc && calc < 24.9) {
+    msgEl.textContent = `${name} you are normal person !`;
+    let utter = new SpeechSynthesisUtterance();
+    utter.lang = "en-US";
+    utter.text = `${name} you are normal person !`;
+    utter.volume = 0.7;
+    window.speechSynthesis.speak(utter);
+  } else if (25 < calc && calc < 29.9) {
+    msgEl.textContent = `${name} you are fat person !`;
+    let utter = new SpeechSynthesisUtterance();
+    utter.lang = "en-US";
+    utter.text = `${name} you are fat person !`;
+    utter.volume = 0.7;
+    window.speechSynthesis.speak(utter);
+  } else if (30 < calc && calc < 6534851276943846) {
+    msgEl.textContent = `${name} you are very fat person !`;
+    let utter = new SpeechSynthesisUtterance();
+    utter.lang = "en-US";
+    utter.text = `${name} you are very fat person !`;
+    utter.volume = 0.7;
+    window.speechSynthesis.speak(utter);
+  } else {
+    msgEl.textContent = `Please fill in the blanks !`
+    let utter = new SpeechSynthesisUtterance();
+    utter.lang = "en-US";
+    utter.text = `Please fill in the blanks !`;
+    utter.volume = 0.7;
+    window.speechSynthesis.speak(utter);
+  }
+
+  nameEl.value = "";
+  heightEl.value = "";
+  weightEl.value = "";
+});
